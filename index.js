ae8ed802ea212f5ea27e4b7d9e4a06966bdf25e9
Dima Voit <dmitriy.voyt@gmail.com>
2023-01-12 12:05:41 +0200

    modified index.js file

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
-function burgerMenu() {
-  const body = document.querySelector("body");
-  const header = document.querySelector("header");
-  const overlay = document.querySelector("#menu-overlay");
-  const menu = document.querySelector("#menu");
-  const burger = document.querySelector(".burger");
-  const navLinks = document.querySelectorAll("#menu li");
+const body = document.querySelector("body");
+const overlay = document.querySelector("#menu-overlay");
+const menu = document.querySelector("#menu");
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelectorAll("#menu li");
 
-  burger.addEventListener("click", () => {
-    burger.classList.toggle("open");
-    body.classList.toggle("overflow-hidden");
-    menu.classList.toggle("hidden");
-    menu.classList.toggle("flex");
-    overlay.classList.toggle("active");
+const clickFunc = () => {
+  body.classList.remove("overflow-hidden");
+  burger.classList.remove("open");
+  menu.classList.remove("flex");
+  menu.classList.add("hidden");
+  overlay.classList.remove("active");
+}
+
+burger.addEventListener("click", () => {
+  body.classList.toggle("overflow-hidden");
+  burger.classList.toggle("open");
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("flex");
+  overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  clickFunc();
+});
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    clickFunc();
   });
-
-  overlay.addEventListener("click", () => {
-    body.classList.remove("overflow-hidden");
-    burger.classList.remove("open");
-    menu.classList.remove("flex");
-    menu.classList.add("hidden");
-    overlay.classList.remove("active");
-  });
-
-  navLinks.forEach((navLink) => {
-    navLink.addEventListener("click", () => {
-      burger.classList.remove("open");
-      menu.classList.remove("flex");
-      navLinks.forEach(el => {
-        el.classList.remove("open");
-      });
-      body.classList.remove("overflow-hidden");
-      menu.classList.add("hidden");
-      overlay.classList.remove("active");
-    });
-  });
-};
-
-burgerMenu();
+});
